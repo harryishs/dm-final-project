00573a200d027f309ef7b70555149a7e3bfd9591
Ted Kassen <tedgkassen@gmail.com>
2016-12-07 21:47:51 -0800

    NN is working

--- predict_mlp.py
+++ predict_mlp.py
@@ -7,64 +7,45 @@ import numpy as np
 trX, trY, teX, teY = load_project_data()
 
 # Parameters
-learning_rate = 0.1e-10000
+learning_rate = 0.5
 training_epochs = 100
 batch_size = 100
 display_step = 1
 
 # Network Parameters
-n_hidden_1 = 512 # 1st layer number of features
-n_hidden_2 = 512 # 2nd layer number of features
-n_hidden_3 = 256
-n_hidden_4 = 128
-n_input = 7  # MNIST data input (img shape: 28*28)
-n_classes = 1 # MNIST total classes (0-9 digits)
+n_input = 7
+n_output = 1
+layers = [n_input, 512, 512, 256, 128, n_output]
 
 # tf Graph input
 x = tf.placeholder("float", [None, n_input])
-y = tf.placeholder("float", [None, n_classes])
+y = tf.placeholder("float", [None, n_output])
 
 # Create model
-def multilayer_perceptron(x, weights, biases):
+def multilayer_perceptron(x, weights, biases, afn):
+    n = len(weights)
+    for i in range(n - 1):
+        x = afn(tf.matmul(x, weights[i]) + biases[i])
+    return tf.matmul(x, weights[n - 1]) + biases[n - 1]
 
-    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-    layer_1 = tf.nn.relu(layer_1)
-
-    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
-    layer_2 = tf.nn.relu(layer_2)
-
-    layer_3 = tf.add(tf.matmul(layer_2, weights['h3']), biases['b3'])
-    layer_3 = tf.nn.relu(layer_3)
-
-    layer_4 = tf.add(tf.matmul(layer_3, weights['h4']), biases['b4'])
-    layer_4 = tf.nn.relu(layer_4)
-
-    out_layer = tf.reduce_sum(tf.matmul(layer_4, weights['out']) + biases['out'])
-
-    return out_layer
-
-# Store layers weight & bias
-weights = {
-    'h1': tf.Variable(tf.zeros([n_input, n_hidden_1])),
-    'h2': tf.Variable(tf.zeros([n_hidden_1, n_hidden_2])),
-    'h3': tf.Variable(tf.zeros([n_hidden_2, n_hidden_3])),
-    'h4': tf.Variable(tf.zeros([n_hidden_3, n_hidden_4])),
-    'out': tf.Variable(tf.zeros([n_hidden_4, n_classes]))
-}
-biases = {
-    'b1': tf.Variable(tf.zeros([n_hidden_1])),
-    'b2': tf.Variable(tf.zeros([n_hidden_2])),
-    'b3': tf.Variable(tf.zeros([n_hidden_3])),
-    'b4': tf.Variable(tf.zeros([n_hidden_4])),
-    'out': tf.Variable(tf.zeros([n_classes]))
-}
+def init_vars(layers):
+    weights, biases = [], []
+    n = len(layers)
+    for i in range(n - 1):
+        lyrstd = np.sqrt(1.0 / layers[i])
+        cur_w = tf.Variable(tf.random_normal([layers[i], layers[i + 1]], stddev=lyrstd))
+        weights.append(cur_w)
+        cur_b = tf.Variable(tf.random_normal([layers[i + 1]], stddev=lyrstd))
+        biases.append(cur_b)
+    return (weights, biases)
 
 # Construct model
-pred = multilayer_perceptron(x, weights, biases)
+weights, biases = init_vars(layers)
+pred = multilayer_perceptron(x, weights, biases, tf.tanh)
 
 # Define loss and optimizer
-cost = tf.square(y - pred)
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
+cost = tf.reduce_sum(tf.nn.l2_loss(pred - y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
 # Initializing the variables
 init = tf.global_variables_initializer()
@@ -81,11 +62,11 @@ with tf.Session() as sess:
         for i in range(total_batch):
             lo = i*batch_size
             hi = (i+1)*batch_size
-            batch_x, batch_y = trX[lo:hi], trY[lo:hi].reshape(batch_size,n_classes)
+            batch_x, batch_y = trX[lo:hi], trY[lo:hi].reshape(batch_size,n_output)
 
             # Run optimization op (backprop) and cost op (to get loss value)
-            _, c = sess.run([optimizer, cost], feed_dict={x: batch_x,
-                                                          y: batch_y})
+            sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
+            c = np.sqrt(sess.run(cost, feed_dict={x: batch_x, y: batch_y}) * 2.0 / total_batch)
             # Compute average loss
             #print(c)
             avg_cost += np.sum(c) / total_batch
@@ -94,5 +75,3 @@ with tf.Session() as sess:
         if epoch % display_step == 0:
             print("Epoch:", '%04d' % (epoch+1), "cost=", \
                 "{:.9f}".format(avg_cost))
-    print("Optimization Finished!")
-    #print(sess.run(weights))
